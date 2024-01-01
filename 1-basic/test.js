27545cfcadcf42860be7f935c88035a7b397890b
SujanBashyal <129775542+SujanBashyal@users.noreply.github.com>
2024-01-01 14:54:36 +0000

    test completed

--- 1-basic/test.js
+++ 1-basic/test.js
@@ -1,1 +1,10 @@
-console.log("Mickle")
+const name="sujan bashyal"
+let roll=15
+ city="palpa"
+ var town="tansen"
+ console.table([name,roll,city,town])
+ 
+  roll=1
+  city="ruphendi"
+  town="manigram"
+  console.table([name,roll,city,town]) 
